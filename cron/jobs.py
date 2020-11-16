ca46bc923b7e66394963cebbfd0ef7d48f107068
luo2083 <1354029556@qq.com>
2020-11-16 10:16:11 +0800

    add cron jobs.

---
+++ cron/jobs.py
@@ -0,0 +1,10 @@
+import time
+import datetime
+import logging
+
+logger = logging.getLogger('django')
+
+def demo():
+    message = 'Job log in crontab, now time is :' + str(datetime.datetime.now())
+    print(message)
+    logger.info(message)
