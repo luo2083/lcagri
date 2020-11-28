2525b9e5a48e202255124a7e42dc1c0ed6191e7a
luo2083 <1354029556@qq.com>
2020-11-28 09:05:32 +0800

    modify settings12

--- backend/settings.py
+++ backend/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'backend',
+        'NAME': 'backend1',
         'USER': 'luo',
         'PASSWORD': '@klj905789',
         'HOST': '127.0.0.1',
